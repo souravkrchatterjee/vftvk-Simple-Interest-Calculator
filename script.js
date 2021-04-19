adbaa88c8a7c63d9eccfae5562a89580b27a51e2
souravkrchatterjee <34605612+souravkrchatterjee@users.noreply.github.com>
2021-04-19 08:17:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 function compute()
 {
-<<<<<<< Updated upstream
-    var principal = document.getElementById("principal").value;
-
-    principal = Number(principal);
-    var rate = document.getElementById("rate").value;
-    rate= parseFloat(rate);
-    var years = document.getElementById("years").value;
-    years=Number(years);
-    var interest = principal*rate*years/100;
-
-    if(validatePrincipal() === true){
-
-    var currentDate = new Date();
-    var currentYear= currentDate.getFullYear();
-    currentYear=Number(currentYear);
-    var futureYear = currentYear + years;
-
-    var result = document.getElementById("result");
-      var output = "If you deposit <mark>" +principal+"</mark>,<br/>"+  "at an interest rate of <mark>" + rate + "</mark>%,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> in the year <mark>" + futureYear + "</mark>";
-
-          result.innerHTML = output;
-}
-}
-
-    function showRange()
-    {
-    var rate = document.getElementById("rate").value;
-    document.getElementById("rate").nextElementSibling.innerHTML = rate +'%';
-
-    }
-=======
 		//Create variables from input
     let principal = document.getElementById("principal").value;
     let rate = document.getElementById("rate").value;
@@ -62,4 +31,3 @@ function rangeReader()
     let sliderValue = document.getElementById("rate").value;
     document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
->>>>>>> Stashed changes
